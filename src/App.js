5e85f8aaaca9f8e06b561b1d98c76e905d183447
tatianasnook <tatsnook@gmail.com>
2023-10-26 17:05:09 -0500

    added travel destinations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
 
 function App() {
+  const [places, setPlaces] = useState(data);
+  const [showText, setShowText] = useState(false);
+  
+  const removePlace = (id) => {
+    let newPlaces = places.filter(place => place.id !== id)
+    setPlaces(newPlaces)
+  } 
+
+  const showDescription = (place) => {
+    place.showMore = !place.showMore
+    setShowText(!showText)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>my travel bucket list - {places.length}</h1>
+      {places.map(place => {
+        const {id, placeName, description, image, showMore} = place
+        
+        return (
+          <div className='container' key={id}>
+            <h2>{placeName}</h2>
+            <img src={image} alt={placeName} width="600px"/>
+            <p>
+              {showMore ? description : description.substring(0, 260) + '...'}
+              <button className='textBtn' onClick={() => showDescription(place)}>
+                {showMore ? 'show less' : 'show more'}
+              </button>
+            </p>
+            <button className='btn' onClick={() => removePlace(id)}>Done</button>
+          </div>
+        )
+      })}
+      <button className='btn deleteBtn' onClick={() => setPlaces([])}>Delete All</button>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 1,
+    placeName: "Greenland",
+    description: "Greenland, an autonomous nation within the Kingdom of Denmark, can truly be said to be a world apart. Icebergs and glaciers are the most famous of Greenland's natural attractions, and the Ilulissat Ice-fjord in Disko Bay is a popular place to visit if you want to see the colossal shapes of blue and white float on an azure sea. Green mountains with beautiful wildflowers, breathtaking fjords, precipitous cliffs, hot springs, and crystal-clear skies perfect for viewing the northern lights are all waiting to be discovered. Seals and whales flourish in the sea, and impressive animals like polar bears and reindeer roam the land. Because of the unique topography, there are virtually no roads between towns, so if you want to explore, it will be by boat, airplane, sled, or snowmobile. In fact, you'll find that some of the best things to do in Greenland are active ones — dogsledding, snowmobiling, hiking, walking on the ice cap, and traveling its waters by boat.",
+    image: "https://www.planetware.com/photos-large/DK/denmark-greenland-northern-lights.jpg",
+    showMore: false
+  },
+  {
+    id: 2,
+    placeName: "Paris",
+    description: "Paris, the capital of France, has a population of over two million people and is known as the most romantic city in the world. Paris is a diverse and sophisticated city that appeals to the wealthy but can also be enjoyed on a budget. In addition to the Eiffel Tower, Paris has countless other gorgeous landmarks and monuments that add to the beauty of the spacious boulevards and their charming cafés. French cuisine is also world-famous, and you can find some of the best of it in Paris. There is world-class shopping and a dynamic fashion scene, and plenty of interesting museums and art galleries. Just outside Paris, there are plenty of fun day trips to take for further insight into the culture and history of France.",
+    image: "https://images.unsplash.com/photo-1612821058477-8b39080e8dee?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    showMore: false
+  },
+  {
+    id: 3,
+    placeName: "Japan",
+    description: "Japan is an island nation, made up of four main islands of Hokkaido, Honshu, Shikoku, and Kyushu, as well as many thousands of smaller islands. Japan is a land of true contrast; rich with centuries-old tradition, the country has ancient, ornate temples and picturesque old towns standing alongside the dazzling lights and technical innovation of ultra-modern cities, like Tokyo and Osaka. Japan's unique blend of ancient and modern is one of the main draws for visitors, who love to explore the modernity of Japanese city life while also experiencing countless natural wonders and learning about the country's rich history. It's a remarkably diverse country just waiting to be discovered, where you can disconnect from the high-tech cities with days spent in serene nature, discovering ancient rituals, shrines and springs.",
+    image: "https://images.unsplash.com/photo-1526481280693-3bfa7568e0f3?auto=format&fit=crop&q=80&w=1742&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    showMore: false
+  },
+  {
+    id: 4,
+    placeName: "Australia",
+    description: "Australia is a huge country! While the interior is arid desert, the coastlines are some of the most beautiful in the world. Australia offers endless outdoor adventures, including scuba diving or snorkeling the Great Barrier Reef, climbing in the Outback, surfing at Byron Bay, hiking the Blue Mountains, bungee jumping, skydiving and more. Cities are spread far across the continent, thus, offering a variety of cultures. Sydney is the business hub, a little more posh. Melbourne is artsy and hip. Cairns is warm and on the Reef. Brisbane is where you’ll find the koalas and kangaroos.",
+    image: "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?auto=format&fit=crop&q=80&w=1740&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    showMore: false
+  },
+  {
+    id: 5,
+    placeName: "New Zealand",
+    description: "New Zealand may be known for its adventure activities — it is, after all, the birthplace of the commercial bungee jump — but there’s a lot more to this extraordinary country than the adrenaline in its veins. Explore remarkable landscapes of fire and ice, from bubbling mud pools and volcanic plains to soaring glaciers and snow-topped mountains. Enjoy pristine sandy beaches and quiet forests disturbed only by birdsong. Add to that the rich native Maori culture, and you have an ideal destination to explore.",
+    image: "https://cdn.kimkim.com/files/a/content_articles/featured_photos/3117192df163b1c655a6081e58961533bd8e1afe/big-317a87546e8649aae22e694d77523453.jpg",
+    showMore: false
+  }
+]
